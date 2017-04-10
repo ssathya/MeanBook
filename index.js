fad1e165f6f07ce4c24943cb4686afd33f6f8d69
Sridhar Sathya <sridhar_0314@live.com>
2017-04-10 14:46:44 -0400

    Basic express application created.

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+app.use('/', (req, res) => {
+    res.status(200).send('Hello world!');
+});
+app.listen(3000);
+console.log('Server running at http://localhost:3000');
+module.exports = app;
